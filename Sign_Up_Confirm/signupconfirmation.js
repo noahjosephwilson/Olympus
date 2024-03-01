929d2346ad0f881cad26f9b757689293ee8fce9f
Carson Wolber <ctw54@cornell.edu>
2024-02-29 20:40:21 -0500

    updated nomanclature

--- Sign_Up_Confirm/signupconfirmation.js
+++ Sign_Up_Confirm/signupconfirmation.js
@@ -1,3 +1,3 @@
 setTimeout(function () {
-    window.location.href = "/Sign Up Name/signuppagename.html";
+    window.location.href = "/Sign_Up_Name/signuppagename.html";
 }, 5000); // 5000 milliseconds = 5 seconds
--- Sign_Up_Username/signuppageusername.js
+++ Sign_Up_Username/signuppageusername.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var submitButton = document.getElementById('submit');
     submitButton.addEventListener('click', function (e) {
         e.preventDefault(); // Prevent the default form submission if it's a submit button
-        window.location.href = '/Sign Up Confirm/signupconfirmation.html';
+        window.location.href = '/Sign_Up_Confirm/signupconfirmation.html';
     });
 });
